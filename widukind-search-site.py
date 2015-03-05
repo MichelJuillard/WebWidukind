7f2cc0e63eb62c2e7ac6d0b9cac30aab2c63c124
Michel Juillard <michel.juillard@mjui.fr>
2015-03-05 12:13:48 +0100

    adding search by series

--- widukind-search-site.py
+++ widukind-search-site.py
@@ -4,24 +4,32 @@ from elasticsearch import Elasticsearch
 import os
 
 app = Flask(__name__)
-#app.debug = True
+app.debug = True
 app.config['SECRET_KEY'] = 'very very secret key key key'
 #app.secret_key = os.urandom(24)
 
-def elasticsearch_query(query):
+def elasticsearch_query_datasets(query):
     es = Elasticsearch(host = "localhost")
-    res = es.search(index = 'widukind', size=20, body={"query": {"query_string": {"query": query}}})
-    print(res)
+    res = es.search(index = 'widukind', doc_type = 'datasets', size=20, body={"query": {"query_string": {"query": query}}})
     results = []
     for hit in res['hits']['hits']:
         s = hit["_source"]
         results.append({"datasetCode": s["datasetCode"], "name": s["name"]})
-    print(results)
+    return results
+
+def elasticsearch_query_series(query):
+    es = Elasticsearch(host = "localhost")
+    res = es.search(index = 'widukind', doc_type = 'series', size=20, body={"query": {"query_string": {"query": query}}})
+    results = []
+    for hit in res['hits']['hits']:
+        s = hit["_source"]
+        results.append({"key": s["key"], "name": s["name"]})
     return results
 
 @app.route('/')
-def hello_world():
-    return render_template('search.html')
+def initial_page():
+    return render_template('search_series.html')
+#    return render_template('search_datasets.html')
 
 @app.route('/facet1', methods = ['POST'])
 def facet1():
@@ -31,19 +39,22 @@ def facet1():
     print(req)
     return redirect('/')
 
-@app.route('/search', methods = ['POST'])
-def search():
+@app.route('/search_datasets', methods = ['POST'])
+def search_datasets():
     query = request.form['query']
-    results = elasticsearch_query(query)
-    session['results'] = results
-    return redirect('/')
-
-@app.route('/disp_results', methods = ['POST'])
-def disp_results():
-    query = request.form['query']
+    results = elasticsearch_query_datasets(query)
     session['query'] = query
-    elasticsearch_query(query)
-    return redirect('/disp_results')
+    session['results'] = results
+    return render_template('search_datasets.html')
+
+@app.route('/search_series', methods = ['POST'])
+def search_series():
+    query = request.form['query']
+    results = elasticsearch_query_series(query)
+    session['query'] = query
+    session['results'] = results
+    return render_template('search_series.html')
+
 
 
 
