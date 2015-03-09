2dc15a350aaccb9cbe1144b694a6497f5332d35a
Michel Juillard <michel.juillard@mjui.fr>
2015-03-09 13:55:50 +0100

    new version of web site with series search and several other features

--- widukind-search-site.py
+++ widukind-search-site.py
@@ -1,7 +1,11 @@
-from flask import Flask, render_template, request, redirect, session
+from flask import Flask, render_template, request, redirect, session, make_response
+from dlstats import configuration
 import pymongo
 from elasticsearch import Elasticsearch
 import os
+import pandas
+import io
+import csv
 
 app = Flask(__name__)
 app.debug = True
@@ -17,6 +21,12 @@ def elasticsearch_query_datasets(query):
         results.append({"datasetCode": s["datasetCode"], "name": s["name"]})
     return results
 
+def mongodb_series_by_key(key):
+    mb = pymongo.MongoClient(**configuration['MongoDB'])
+    print(key)
+    series = mb.widukind.series.find_one({'key': key},{'revisions': 0})
+    return series
+    
 def elasticsearch_query_series(query):
     es = Elasticsearch(host = "localhost")
     res = es.search(index = 'widukind', doc_type = 'series', size=20, body={"query": {"query_string": {"query": query}}})
@@ -55,10 +65,54 @@ def search_series():
     session['results'] = results
     return render_template('search_series.html')
 
+@app.route('/print_series', methods = ['GET', 'POST'])
+def print_series():
+    key = next(request.args.lists())[0]
+    series = mongodb_series_by_key(key)
+    session['name'] = series['name']
+    session['key'] = series['key']
+    session['dimensions'] = series['dimensions']
+    sd = pandas.Period(series['startDate'],freq=series['frequency'])
+    values = []
+    for val in series['values']:
+        values.append([str(sd), val])
+        sd += 1
+    session['elements'] = values
+    return render_template('print_series.html')
 
+@app.route('/plot_series', methods = ['GET', 'POST'])
+def plot_series():
+    key = next(request.args.lists())[0]
+    series = mongodb_series_by_key(key)
+    session['name'] = series['name']
+    session['key'] = series['key']
+    sd = pandas.Period(series['startDate'],freq=series['frequency'])
+    values = []
+    for val in series['values'][:-1]:
+        values.append([sd.to_timestamp(), val, '+'])
+        sd += 1
+    values.append([sd.to_timestamp(), val, ''])
+    session['elements'] = values
+    return render_template('plot_series.html')
 
-
+@app.route('/download_series', methods = ['GET', 'POST'])
+def download_series():
+    key = next(request.args.lists())[0]
+    series = mongodb_series_by_key(key)
+    sd = pandas.Period(series['startDate'],freq=series['frequency'])
+    values = []
+    for val in series['values']:
+        values.append([str(sd), val])
+        sd += 1
+    csv_output = io.StringIO()
+    writer = csv.writer(csv_output, quoting=csv.QUOTE_NONNUMERIC)
+    writer.writerows(values)
+    
+    response = make_response(csv_output.getvalue())
+    response.headers["Content-disposition"] = "attachment; filename="+key+".csv"
+    
+    return response
 
 if __name__ == '__main__':
- #   app.debug = True
+    app.debug = True
     app.run()
