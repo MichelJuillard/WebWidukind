6ba6b19d3e3d54ad26e84f1b0f249ade07ab748a
Michel Juillard <michel.juillard@mjui.fr>
2015-03-16 09:45:18 +0100

    adding search for series in one dataset

--- widukind-search-site.py
+++ widukind-search-site.py
@@ -12,14 +12,21 @@ app.debug = True
 app.config['SECRET_KEY'] = 'very very secret key key key'
 #app.secret_key = os.urandom(24)
 
-def elasticsearch_query_datasets(query):
+def elasticsearch_query_datasets(query,filter={}):
     es = Elasticsearch(host = "localhost")
-    res = es.search(index = 'widukind', doc_type = 'datasets', size=20, body={"query": {"query_string": {"query": query}}})
+    res = es.search(index = 'widukind', doc_type = 'datasets', size=20, body=form_es_query(query,filter))
     results = []
     for hit in res['hits']['hits']:
         s = hit["_source"]
         results.append({"datasetCode": s["datasetCode"], "name": s["name"]})
     return results
+
+def elasticsearch_get_dataset(datasetCode):
+    es = Elasticsearch(host = "localhost")
+    res = es.search(index = 'widukind', doc_type = 'datasets', size=20, body={"filter": {"term": {'datasetCode': datasetCode}}})
+    if len(res['hits']['hits']):
+        print(res['hits']['hits'][0]['_source'])
+        return res['hits']['hits'][0]['_source']
 
 def mongodb_series_by_key(key):
     mb = pymongo.MongoClient(**configuration['MongoDB'])
@@ -27,14 +34,37 @@ def mongodb_series_by_key(key):
     series = mb.widukind.series.find_one({'key': key},{'revisions': 0})
     return series
     
-def elasticsearch_query_series(query):
+def elasticsearch_query_series(query,filter={}):
     es = Elasticsearch(host = "localhost")
-    res = es.search(index = 'widukind', doc_type = 'series', size=20, body={"query": {"query_string": {"query": query}}})
+    res = es.search(index = 'widukind', doc_type = 'series', size=20, body=form_es_query(query,filter))
     results = []
     for hit in res['hits']['hits']:
         s = hit["_source"]
         results.append({"key": s["key"], "name": s["name"]})
     return results
+
+def form_es_query(query,filter):
+    if len(filter):
+        if len(query):
+            es_query = {
+                'query': {
+                    'filtered': {
+                        'query': {
+                            'query_string': {
+                                'query': query
+                            }
+                        },
+                        'filter': {
+                            'term': filter
+                        }
+                    }
+                }
+            }
+        else:
+            es_query = {"filter": {"term": filter}}
+    else:
+        es_query = {"query": {"query_string": {"query": query}}}
+    return es_query
 
 @app.route('/')
 def initial_page():
@@ -62,14 +92,23 @@ def search_datasets():
     session['results'] = results
     return render_template('search_datasets.html')
 
-@app.route('/search_series_in_datasets', methods = ['GET','POST'])
-def search_series_in_datasets():
-    query = request.form['query']
-    dataset = query['name']
-    results = elasticsearch_query_series(query)
-    print(results)
+@app.route('/search_series_in_dataset', methods = ['GET','POST'])
+def search_series_in_dataset():
+    if 'query' in request.form:
+        query = request.form['query']
+    else:
+        query = ''
+    datasetCode = request.form['datasetCode']
+    filter = {'datasetCode': datasetCode}
+    results = elasticsearch_query_series(query,filter)
+    dataset = elasticsearch_get_dataset(datasetCode)
+    print(dataset)
     session['query'] = query
+    session['filter'] = filter
     session['results'] = results
+    session['datasetCode'] = datasetCode
+    session['datasetName'] = dataset['name']
+    session['dataset'] = dataset
     return render_template('search_series_in_dataset.html')
 
 @app.route('/search_series', methods = ['GET','POST'])
